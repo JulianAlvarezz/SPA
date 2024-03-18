eccd270ab7ce2a7a3ecb0f5a1172accca942499d
“Julian” <jalvarezgaviriajulian@cesde.net>
2024-03-17 22:11:16 -0500

    ejercicio spa

---
+++ app/App.js
@@ -0,0 +1,5 @@
+
+export const App = () => {
+let divRoot =  document.querySelector("#root")
+divRoot.innerHTML = "<h1> Nuestra primera web SPA </h1>"
+}
---
+++ app/componentes/Header.js
@@ -0,0 +1,10 @@
+
+export const Header = () => {
+    let nav = document.createElement("nav");
+
+    nav.innerHTML =  
+    
+    <a href = "#/">Home</a>
+    
+
+}
